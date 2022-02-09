579f4330e427bfb399d685b035fbd3d69f500822
Amobi Gordon Maka <amobimak@gmail.com>
2022-02-09 15:07:28 -0500

    functions guide

---
+++ PY_Functions/functions_A.py
@@ -0,0 +1,16 @@
+# Reusable pieces of code are what we call "Functions"
+# def stands for define function
+# Built-in functions we've already been using: print(), int(), float(), str(), input(), type().
+# Example 1
+def thing():
+  print('Rita')
+
+thing()
+print('Blah blah blah, Tidal Wave')
+
+big = max('Hello world')
+print(big)
+
+tiny = min('Hello world')
+print(tiny)
+# No return value, why? B/c the blank is the smallest value.
