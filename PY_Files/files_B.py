03cfc93bb504e662829b0281827ea4c6cddb66f5
Amobi Gordon Maka <amobimak@gmail.com>
2022-02-14 13:20:17 -0500

    Update files_B.py

--- PY_Files/files_B.py
+++ PY_Files/files_B.py
@@ -21,6 +21,25 @@
 # The for loop doesnt throw the \n new lines away
 # New line is considered white space and can be stripped using rstrip().
 
+# Skipping with continue...
+# for line in fhand:
+  # line = line.rstrip()
+  # if not line.startswith('From: ') :
+    # continue
+  # print(line)
+# We can conveniently skip a line/iteration by using the continue statement
+
+# Using in to select lines
+# We can look for a string anywhere in a line as our selection criteria 
+# for line in fhand:
+  # line = line.rstrip()
+  # if not 'selection criteria' in line :
+    # contine
+  # print(line)
+
+# Filename can also be inputted as an input prompt
+# for bad file names, or files that do not exist, you can include try/except(quit()) for insurance. Without quit it would continue. This allows the entire python program to end without a traceback.  
+
 
 
 
