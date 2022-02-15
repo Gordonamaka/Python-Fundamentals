9021206975632ff7fdaae9504b14252c11a3d1a0
Amobi Gordon Maka <amobimak@gmail.com>
2022-02-15 11:29:07 -0500

    To do list for Repo

--- PY_Dictionaries/dictionaries_A.py
+++ PY_Dictionaries/dictionaries_A.py
@@ -1,2 +1,5 @@
 # Create a Javascript/Python Algorithms and Data Structures Repo
-# Try to upload more to typescript repo this month, keep it fresh.
+# Try to upload more to Typescript repo this month, keep it fresh.
+# Upload a typescript Boilerplate for ...
+# Build a Django RestApi Boilerplate Repo
+# Check the D3 library on Freecodecamp for data visualization (See if it's applicable w/ Django)
