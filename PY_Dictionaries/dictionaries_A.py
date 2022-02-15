d01a0db22e4155dbb9726407c71d37e422aa91fe
Amobi Gordon Maka <amobimak@gmail.com>
2022-02-15 12:02:30 -0500

    Update dictionaries_A.py

--- PY_Dictionaries/dictionaries_A.py
+++ PY_Dictionaries/dictionaries_A.py
@@ -1,8 +1,9 @@
 # Create a Javascript/Python Algorithms and Data Structures Repo
 # PERN-Stack Boilerplate
 # MERN-Stack Boilerplate (MongoDB) 
+# Build a Django RestApi Boilerplate Repo
+# Python-Django Tech-Stack Boilerplate (MySQL, build one for Flask if there's time) 
+# Check the D3 library on Freecodecamp for data visualization (See if it's applicable w/ Django)
 # Try to upload more to Typescript repo this month, keep it fresh.
 # Upload a typescript Boilerplate for ReactJS (Check about NextJs)
-# Build a Django RestApi Boilerplate Repo
-# Check the D3 library on Freecodecamp for data visualization (See if it's applicable w/ Django)
 # Visualize Data w/ Python & D3
