db02cd2911ef38fff75f3e6663d77d0b303054a7
Amobi Gordon Maka <amobimak@gmail.com>
2022-02-15 12:13:34 -0500

    Update dictionaries_A.py

--- PY_Dictionaries/dictionaries_A.py
+++ PY_Dictionaries/dictionaries_A.py
@@ -1,6 +1,7 @@
 # Create a Javascript/Python Algorithms and Data Structures Repo
 # PERN-Stack Boilerplate
 # MERN-Stack Boilerplate (MongoDB) 
+# https://www.freecodecamp.org/news/whats-boilerplate-and-why-do-we-use-it-let-s-check-out-the-coding-style-guide-ac2b6c814ee7/
 # Build a Django RestApi Boilerplate Repo
 # Python-Django Tech-Stack Boilerplate (MySQL, build one for Flask if there's time) 
 # Check the D3 library on Freecodecamp for data visualization (See if it's applicable w/ Django)
