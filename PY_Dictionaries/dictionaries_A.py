f0ec90887cdc96bdb9834398ab0d588cfcd4fb23
Amobi Gordon Maka <amobimak@gmail.com>
2022-02-15 12:33:32 -0500

    Update dictionaries_A.py

--- PY_Dictionaries/dictionaries_A.py
+++ PY_Dictionaries/dictionaries_A.py
@@ -1,10 +1,22 @@
-# Create a Javascript/Python Algorithms and Data Structures Repo
-# PERN-Stack Boilerplate
-# MERN-Stack Boilerplate (MongoDB) 
 # https://www.freecodecamp.org/news/whats-boilerplate-and-why-do-we-use-it-let-s-check-out-the-coding-style-guide-ac2b6c814ee7/
-# Build a Django RestApi Boilerplate Repo
-# Python-Django Tech-Stack Boilerplate (MySQL, build one for Flask if there's time) 
+
+# PERN-Stack Boilerplate/Scaffolding
+
+# Create a Javascript/Python Algorithms and Data Structures Repo when you reach that section in CTCI
+
+# MERN-Stack Boilerplate/Scaffolding (MongoDB) 
+
+# Build a Python-Django RestApi Boilerplate Repo : (MySQL / MongoDB - SQL would be faster to learn.) : https://www.educba.com/sql-server-vs-postgresql/
+
+# Build a Typescript React Boilerplate 
+# Python - build a Flask boilerplate if there's time
+
+# Build your website: https://www.quora.com/What-kind-of-projects-should-one-upload-on-GitHub-Could-they-be-small-projects-or-important-code-snippets-which-could-be-useful-to-someone-in-the-future#:~:text=You%20can%20put%20up%20anything,a%20modicum%20of%20marketable%20skills.
+
+# Check out Free Cloud Computing learning for Data Infrastructure : https://medium.com/javarevisited/10-free-courses-to-learn-cloud-computing-for-beginners-4f3cd984ddb1 
+
 # Check the D3 library on Freecodecamp for data visualization (See if it's applicable w/ Django)
+
 # Try to upload more to Typescript repo this month, keep it fresh.
-# Upload a typescript Boilerplate for ReactJS (Check about NextJs)
+
 # Visualize Data w/ Python & D3
