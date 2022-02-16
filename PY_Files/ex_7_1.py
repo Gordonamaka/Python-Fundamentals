1acdbc979593ec17bfee12670112cab7c2b41e05
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-02-16 15:01:30 -0500

    Update ex_7_1.py

--- PY_Files/ex_7_1.py
+++ PY_Files/ex_7_1.py
@@ -3,14 +3,9 @@ finput = input('Enter files name: ')
 fhand = open(finput)
 for line in fhand:
   line = line.rstrip()
-  print('Line:', line)
-  if line == '' :
-    print('Skip Blank')
-    continue
   wds = line.split()
-  print('Words:', wds)
-  if wds[0] != 'From' :
-    print('Ignore')
+  if len(wds) < 3 or wds[0] != 'From' :
     continue
   print(wds[2])
 
+# if the left side of the or is true, it ignores the left condition. Order matters.
