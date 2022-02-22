cc76c8c2973b1b822aec1f1f0d965299932c49c2
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-02-22 17:05:30 -0500

    Create http_B.py

---
+++ PY_Networking/http_B.py
@@ -0,0 +1,12 @@
+# Application Protocol
+
+"""
+Since TCP (and python) gives us a reliable socket, we want to think about what we want to do with the socket & what problem do we want to solve.
+"""
+
+# Example Protocol
+# http://www.dr-chuck.com/page1.htm
+# ^Protocol     ^Host      ^Document
+
+# Get requests is the type of HTTP request is usually used to access a website. (Other CRUD operations are accessible, but get requests are the most common)
+
