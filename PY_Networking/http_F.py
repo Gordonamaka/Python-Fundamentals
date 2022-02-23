71a88fe0e5f6b12780748c54c15af68163dd0ad4
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-02-23 13:41:58 -0500

    Create http_F.py

---
+++ PY_Networking/http_F.py
@@ -0,0 +1,8 @@
+# Web Scrapping in Python
+
+"""
+- When a program or script pretends to be a browser and retrieves web pages, looks at those web pages, extracts information, and hten looks at more web pages
+
+- Search engines scrape web pages - we call this "spidering the web" or "web crawling"
+"""
+
