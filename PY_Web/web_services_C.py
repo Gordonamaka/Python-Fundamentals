9e05669408b4697c8673221c98d7fcae108cd040
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-02-25 13:23:56 -0500

    Update web_services_C.py

--- PY_Web/web_services_C.py
+++ PY_Web/web_services_C.py
@@ -1,6 +1,6 @@
 # Web Services: JSON
 
-# Python to process JSON
+# Python to process JSON - ex.1
 import json
 data = '''{
   "name" : "Chuck",
@@ -13,7 +13,28 @@ data = '''{
   }
 }'''
 
-info = json.loads(data)
+info = json.loads(data) # Info is a dictionary
 print('Name:', info["name"])
 print('Hide:', info["email"]["hide"])
 
+# Ex.2
+input = '''[
+  { 
+  "id" : "001",
+  "x" : "2",
+  "name" : "Chuck"
+  } ,
+  { 
+  "id" : "009",
+  "x" : "7",
+  "name" : "Chuck"
+  }
+]'''
+
+# Json represents data as nested 'lists" and 'dictionaries' (You can map it into simplier data structures to manipulate).
+infotwo = json.loads(input)
+print('User count:', len(info))
+for item in info:
+  print('Name', item['name'])
+  print('Id', item['id'])
+  print('Attribute', item['x'])
