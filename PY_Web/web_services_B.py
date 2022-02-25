5bc10e6ca9f3edc96cfa5bf2aabff6f563f355db
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-02-25 13:16:09 -0500

    Web Service con't

--- PY_Web/web_services_B.py
+++ PY_Web/web_services_B.py
@@ -9,3 +9,40 @@ Often used to specify a "contract" between systems - "My system will only accept
 
 If a particular piece of XML meets the specification of the Schema - it is said to "validate"
 """
+
+# Example 1
+import xml.etree.ElementTree as ET
+data = '''<person>
+<name>Chuck</name>
+<phone type="int1">
++1 734 3030 4456
+</phone>
+<email hide="yes" />
+<person>'''
+
+tree = ET.fromstring(data)
+print('Name:', tree.find('name').text)
+print('Attr:', tree.find('email').get('hide'))
+
+
+# Example 2
+input = '''<stuff>
+  <users>
+    <user x="2">
+      <id>001</id>
+      <name>Chuck</name>
+    </user>
+    <user x="7">
+      <id>009</id>
+      <name>Brent</name>
+    </user>
+  </users>
+</stuff>'''
+
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+for item in lst:
+  print('Name', item.find('name').text)
+  print('Id', item.find('id').text)
+  print('Attribute', item.get("x"))
