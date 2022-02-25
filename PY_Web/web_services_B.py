4e320549ee458263f40df6f19ec61dd4a0060e00
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-02-25 12:39:46 -0500

    Create web_services_B.py

---
+++ PY_Web/web_services_B.py
@@ -0,0 +1,11 @@
+# XML Schema
+
+"""
+Description of the legal format of an xml document
+
+Expressed in terms of constraints on the structure and content of documents
+
+Often used to specify a "contract" between systems - "My system will only accept XML that conforms to this particular Schema."
+
+If a particular piece of XML meets the specification of the Schema - it is said to "validate"
+"""
