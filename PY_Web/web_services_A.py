e387e51bbb91824ad26e913779e84d748c686b6b
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-02-25 12:19:45 -0500

    Create web_services_A.py

---
+++ PY_Web/web_services_A.py
@@ -0,0 +1,16 @@
+# XML - eXtensible Markup Language
+
+"""
+Primary purpose is to help information systems share structures data
+It started as a simplified subset of the Standard Generalized Markup Language (SGML), and is designed to be relatively human-legible
+
+XML Terminology:
+
+Serial/De-Serialize - Convert data in one program into a commmon format that can be stored and/or transmitted between systems in a programming langugage-independent manner.
+
+(Basic HTML/JS HTML(JSX))
+Attributes - Keyword/value pairs on the opening tag of XML.
+Tags - Indicate the beginning and ending of elements.
+(Text, elements and attributes are all different)
+"""
+
