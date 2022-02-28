105bdf183831d23c8f2aebb78f5bf92facbea683
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-02-28 17:26:09 -0500

    Create database_B.py

---
+++ PY_Databases/database_B.py
@@ -0,0 +1,14 @@
+# Database Design
+
+"""
+Database design is an art form of its own with particular skills and experience.
+
+Our goal is to avoid the really bad mistakes and design clean and easily understood databases
+
+Others may performance turn things later
+
+Database design starts with a picture
+
+Basic Rule: Never put the same string data in twice - use a relationship instead.
+"""
+
