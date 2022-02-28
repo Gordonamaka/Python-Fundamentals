e2a8de51f2b3308d2a1cb5a1e2393b490f94a4e2
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-02-28 17:48:22 -0500

    Update database_B.py

--- PY_Databases/database_B.py
+++ PY_Databases/database_B.py
@@ -12,3 +12,20 @@ Database design starts with a picture
 Basic Rule: Never put the same string data in twice - use a relationship instead.
 """
 
+# Database Normalization (3NF)
+"""
+Do not replicate data - reference data - point at data
+Use integers for keys and for references
+Add a special 'key' column to each table which we will make references to. By convention, many programmers call this column 'id'
+-
+Primary key - Generally an integer auto-increment field
+Logical key - What the outside world uses for lookup
+Foreign Key - Generally an integer key pointing to a row in another table
+-
+Best Practices :
+Never use your logical key as the primary key
+
+Logical keys can and do change, albeit slowly
+
+Relationships that are based on matching string feilds are less efficient than integers
+"""
