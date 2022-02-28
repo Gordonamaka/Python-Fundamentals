e41a9a4357e19115b2a17017e00f870b5e91a345
Amobi Gordon Maka <89036458+Gordonamaka@users.noreply.github.com>
2022-02-28 14:17:21 -0500

    Create objects_B.py

---
+++ PY_Objects/objects_B.py
@@ -0,0 +1,17 @@
+# A Sample Class - OOP Priniciples
+
+
+# This is the template for making PartyAnimal objects
+class PartyAnimal:
+    x = 0
+    # Each PartyAnimal object has a bit of data
+    # Self: line 17
+    def party(self):
+      self.x = self.x + 1
+      print("So far", self.x)
+
+# Construct a PartyAnimal object and store in 'an'
+an = PartyAnimal()
+an.party()
+an.party() # PartyAnimal.party(an)
+an.party() # Self becomes an alias of an everytime it's called (invocation)
